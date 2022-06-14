1e1fbbc0bc467984a9ef7a6c3ce1a466c6a52218
Rithwik15 <107486046+Rithwik15@users.noreply.github.com>
2022-06-14 06:47:01 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var box
+
+function setup() {
+  createCanvas(800,400);
+ box =  createSprite (100,100,10,10);
+}
+
+function draw() {
+  background ("black");  
+if (keyIsDown(UP_ARROW)){
+box.position.y= box.position.y-5 
+}
+if (keyIsDown(DOWN_ARROW)){
+  box.position.y= box.position.y+5 ;
+  }
+  if (keyIsDown(RIGHT_ARROW)){
+    box.position.x= box.position.x+5 
+    }
+    if (keyIsDown(LEFT_ARROW)){
+      box.position.x= box.position.x-5 
+      }
+
+  drawSprites();
+}
